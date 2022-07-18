441b3d02fcaa6ac0045d5290f23780b22167af36
roman <roma@octarine.com.ua>
2022-07-18 18:13:07 +0300

    add image

--- app/Http/Requests/EditUserRequest.php
+++ app/Http/Requests/EditUserRequest.php
@@ -33,7 +33,7 @@ class EditUserRequest extends FormRequest
     {
         return [
             'name' => 'max:255',
-            'description' => 'text',
+            'description' => 'string|nullable',
             'login' => 'min:4|max:16|unique:users,login,'.auth()->user()->login,
             'password' => 'string|min:8|max:191',
             'avatar' => 'file'
